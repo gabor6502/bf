ccd173a75a99e7b750691d9c01f9d237562e1ba6
gabor6502 <elan.gabor@gmail.com>
2025-08-13 10:25:16 -0500

    added note to readme about development, also tried to make compiler a bit easier to read

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BF Interpreter
 
-## Breif
+## Breif (currently under development)
 A lighweight c program that uses the [brainfuck interpreter by Kang Seonghoon](https://gist.github.com/lifthrasiir/596667) to run any `.bf` files from the command line.
 
 ## Compilation
--- brainfuck.c
+++ brainfuck.c
@@ -7,7 +7,7 @@ main(a,b)
 {
     char * v = 1 [d = b];
     for(; c = *v++ % 93;)
-        for(b = c&2 , b = c%7 ? a && (c&17?c&1?(*r+=b-1) : (r+=b-1) : syscall(4-!b,b,r,1),0) : v; b&&c|a**r; v = d)
+        for(b = c&2 , b = c%7 ? a && (c&17 ? c&1? (*r += b-1) : (r += b-1) : syscall(4-!b, b, r, 1), 0) : v; b && c|a**r; v = d)
         main(!c, &a);
         d = v;
 }
