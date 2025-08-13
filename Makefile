9e2c1281fd1e3e1b7df6360c2cfadece67e897fd
gabor6502 <elan.gabor@gmail.com>
2025-08-13 09:59:33 -0500

    program reading in progress

--- Makefile
+++ Makefile
@@ -1,14 +1,7 @@
-
-# compiles the bf interpreter
-bf:
-	gcc -m32 brainfuck.c -o bf
-
-# compiles a main program to launch bf interpreter with code from a .bf file
 all:
+	gcc -m32 brainfuck.c -o bf
 	gcc main.c -o main
 
-clean-bf:
-	rm -f bf
-
 clean:
+	rm -f bf
 	rm -f main
--- main.c
+++ main.c
@@ -1,6 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
 
-int main()
+char * readFile(const char * bfFile);
+char * getProgramName(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
 {
+    char * programName = getProgramName(argc, argv);
+    char * program = readFile(programName);
+
+    // test
+    printf("%s\n", program);
+
+    free(program);
 
     return 0;
+}
+
+char * getProgramName(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        return argv[1];
+    }
+    else
+    {
+        fprintf(stderr, "Invalid arguments supplied.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+char * readFile(const char * bfFile)
+{
+    char * contents;
+    int length;
+    FILE * file;
+
+    file = fopen(bfFile, "rb");
+
+    if (file)
+    {
+        fseek(file, 0, SEEK_END);
+        length = ftell(file);
+        
+        contents = (char*) malloc(sizeof(char) * length);
+
+        fseek(file, 0, SEEK_SET);
+        fread(contents, 1, length, file);
+
+        fclose(file);
+    }
+    else 
+    {
+        fprintf(stderr, "Couldn't open file %s\n", bfFile);
+        exit(EXIT_FAILURE);
+    }
+
+    return contents;
 }
